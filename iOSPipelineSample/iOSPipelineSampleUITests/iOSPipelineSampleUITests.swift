36bcd49e1ddad41aa2208aeb332a47d4d4f23b41
Prasad Prabhu <prasadprabhu1989@gmail.com>
2020-05-13 14:19:30 +0530

    dsssd

--- iOSPipelineSample/iOSPipelineSampleUITests/iOSPipelineSampleUITests.swift
+++ iOSPipelineSample/iOSPipelineSampleUITests/iOSPipelineSampleUITests.swift
@@ -22,6 +22,7 @@ app = XCUIApplication()
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
     func testUI(){
           app.launch()
@@ -29,21 +30,5 @@ app = XCUIApplication()
       }
 
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+   
 }
